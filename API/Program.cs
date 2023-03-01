b5d6dd76b921b3012db6b5f65988f4b570996d25
Lawliet <coffeelancerr@gmail.com>
2023-03-01 19:37:09 +0300

    AddedController

--- API/Program.cs
+++ API/Program.cs
@@ -1,12 +1,19 @@
+using API.Services;
 using DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
+builder.Services.AddControllers();
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 
+builder.Services.AddScoped<ICoffeeShopService, CoffeeShopService>();
+
 var app = builder.Build();
+
+app.MapControllers();
 
 app.Run();
